0ea518db27e07f2977ce3a841e2cb485eb84af5b
Kanishk Chinna <kanishkchinna@gmail.com>
2023-01-11 17:32:42 -0800

    displays announcements

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+
+// MARKED ISSUE: does not live update announcements
 const HomeScreen = ({ route }) => {
-    console.log(route.params.email);
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    const [email, setEmail] = useState(route.params.email);
+    const [announcements, setAnnouncements] = useState([{}]);
+    useEffect(() => {
+        let url = "http://192.168.87.50:3001/announcement/";
+        try {
+            axios.get(url).then(res => {
+                if (res.data) {
+                    setAnnouncements(res.data.sortedData);
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+    return (
+        <View>
+            {announcements.map(data => {
+                return (
+                    <View key={data.uuid}>
+                        <Text>{data.message}</Text>
+                        <Text>{data.timestamp}</Text>
+                        <Text>--------------</Text>
+                    </View>
+                );
+            })}
+        </View>
+    )
 }
 
 export default HomeScreen
