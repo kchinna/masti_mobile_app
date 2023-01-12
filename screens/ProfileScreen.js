5ade5695c941f6c21bfcc0fcddf33d32168be982
Kanishk Chinna <kanishkchinna@gmail.com>
2023-01-11 17:12:19 -0800

    profile screen displays user data

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const ProfileScreen = ({ route }) => {
-  return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
-  )
+    const [email, setEmail] = useState(route.params.email);
+    const [userData, setUserData] = useState({});
+    useEffect(() => {
+        let url = "http://192.168.87.50:3001/participant/" + email;
+        try {
+            axios.get(url).then(res => {
+                if (res.data) {
+                    setUserData(res.data.item);
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+    console.log(userData)
+    return (
+        <View>
+            <Text>{userData.name}</Text>
+            <Text>{userData.team}</Text>
+            <Text>{userData.hotel}</Text>
+            <Text>{userData.stamp}</Text>
+            <Text>{userData.diet}</Text>
+        </View>
+    )
 }
 
 export default ProfileScreen
