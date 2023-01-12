49e3a525d182090dc1729bd97643f8a10fd455fd
Kanishk Chinna <kanishkchinna@gmail.com>
2023-01-11 16:15:01 -0800

    nested navigation for tab navigator

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import NavContainer from './components/NavContainer';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
     <NavigationContainer styles={styles.container}>
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen options={{ headerShown: false }} name="Navigation" component={NavContainer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
