0e02b861a40bb5bab07b5c733d29daf6f8579641
Kanishk Chinna <kanishkchinna@gmail.com>
2023-01-11 17:23:04 -0800

    notify user when incorrect username/password

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,103 +4,109 @@ import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpaci
 import axios from 'axios';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [authed, setAuthed] = useState('');
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  const handleLogin = async () => {
-    let url = "http://192.168.87.50:3001/login/" + email.toLowerCase() + "/" + password;
-    console.log(url)
-    try {
-        axios.post(url).then(res => {
-            if (res.data) {
-                navigation.navigate("Navigation", { email: email.toLowerCase() })
-            }
-        })
-    } catch (error) {
-        console.log(error);
+    const handleLogin = async () => {
+        let url = "http://192.168.87.50:3001/login/" + email.toLowerCase() + "/" + password;
+        console.log(url)
+        try {
+            axios.post(url).then(res => {
+                if (res.data) {
+                    setAuthed("")
+                    navigation.navigate("Navigation", { email: email.toLowerCase() })
+                }
+                else {
+                    setAuthed("Incorrect Username or Password");
+                }
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding"
-    >
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        />
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={handleLogin}
-          style={styles.button}
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
         >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
-  )
+            <Text>{authed}</Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                />
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleLogin}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     inputContainer: {
-      width: '80%'
+        width: '80%'
     },
     input: {
-      backgroundColor: 'white',
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 5,
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
     },
     buttonContainer: {
-      width: '60%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 40,
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
     },
     button: {
-      backgroundColor: '#0782F9',
-      width: '100%',
-      padding: 15,
-      borderRadius: 10,
-      alignItems: 'center',
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
     },
     buttonOutline: {
-      backgroundColor: 'white',
-      marginTop: 5,
-      borderColor: '#0782F9',
-      borderWidth: 2,
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#0782F9',
+        borderWidth: 2,
     },
     buttonText: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 16,
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
     },
     buttonOutlineText: {
-      color: '#0782F9',
-      fontWeight: '700',
-      fontSize: 16,
+        color: '#0782F9',
+        fontWeight: '700',
+        fontSize: 16,
     },
-  })
+})
