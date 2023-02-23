ce5e25b3c6e89fcb7f90e1666e82d74a8691c6b3
Kanishk Chinna <kanishkchinna@gmail.com>
2023-02-23 14:41:30 -0500

    temporarily show data in text format

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -6,11 +6,11 @@ const ScheduleScreen = ({ route }) => {
     const [email, setEmail] = useState(route.params.email);
     const [events, setEvents] = useState([{}]);
     useEffect(() => {
-        let url = "http://192.168.87.50:3001/participant/" + email;
+        let url = "https://masti-dynamodb-apis-pearl.vercel.app/participant/" + email;
         try {
             axios.get(url).then(res => {
                 if (res.data) {
-                    url = "http://192.168.87.50:3001/schedule/" + res.data.item.team;
+                    url = "https://masti-dynamodb-apis-pearl.vercel.app/schedule/" + res.data.item.team;
                     try {
                         axios.get(url).then(res => {
                             if (res.data) {
